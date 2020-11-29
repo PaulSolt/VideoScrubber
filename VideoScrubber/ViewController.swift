fd254c764adf2af0ffd85c4b8f07c6cf8a4d31e1
Paul Solt <PaulSolt@gmail.com>
2020-11-29 12:33:15 -0500

    Initial Commit

---
+++ VideoScrubber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoScrubber
+//
+//  Created by Paul Solt on 11/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
